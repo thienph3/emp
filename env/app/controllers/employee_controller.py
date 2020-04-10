748e6a2132ff2a19c9d653c24c0e3c61430bd88a
thien-pham <thien.pham@be.xyz>
2020-04-11 06:22:13 +0700

    update add_employee page

--- env/app/controllers/employee_controller.py
+++ env/app/controllers/employee_controller.py
@@ -8,6 +8,6 @@ def index():
         if form.validate_on_submit():
             flash('Nhân viên {}, số CMND: {} khởi tạo thành công.'.format(form.name.data, form.cmnd.data))
             return redirect('/')
-        flash('Có gì đó sai sai.')
-        return render_template('employee/index.html', form=form)
+        #flash('Có gì đó sai sai.')
+        #return render_template('employee/index.html', form=form)
     return render_template('employee/index.html', form=form)
--- env/app/models/employee_form.py
+++ env/app/models/employee_form.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class EmployeeForm(FlaskForm):
-    name = StringField('Họ tên', validators=[DataRequired()])
-    cmnd = StringField('CMND', validators=[DataRequired()])
+    name = StringField('Họ tên', validators=[DataRequired(message=('Chưa nhập họ tên'))])
+    cmnd = StringField('CMND', validators=[DataRequired(message=('Chưa nhập CMND'))])
     phone = StringField('Số điện thoại')
     company = StringField('Nhà thầu')
     reason = StringField('Lí do')
-    dueDate = DateField('Ngày hết hạn', validators=[DataRequired()], format='%d/%m/%Y')
+    dueDate = DateField('Ngày hết hạn', validators=[DataRequired(message=('Chưa nhập ngày hết hạn'))], format='%d/%m/%Y')
     image = StringField('Ảnh chân dung')
     submit = SubmitField('Thêm nhân viên mới')
