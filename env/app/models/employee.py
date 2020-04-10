238d955e2d0820656f877f0fb0d3c17ecb4eaf67
thien-pham <thien.pham@be.xyz>
2020-04-11 00:01:10 +0700

    update Model.Employee

--- env/app/models/employee.py
+++ env/app/models/employee.py
@@ -3,3 +3,20 @@ class Employee:
         self.ID = 0
         self.Name = Name
         self.Status = 0
+
+    @staticmethod
+    def insert(employee):
+        return 0
+    
+    @staticmethod
+    def getEmployeesByStatus(status):
+        return []
+    
+    @staticmethod
+    def updateEmployeeByID(employee):
+        return employee.ID
+
+    @staticmethod
+    def getEmployeeByID(id):
+        r = Employee('')
+        return r
