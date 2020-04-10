006eb51c303856f90dd9a6cdbc602fbd9c8bbdde
thien-pham <thien.pham@be.xyz>
2020-04-10 22:45:18 +0700

    update structure

---
+++ env/README.md
@@ -0,0 +1,6 @@
+For Windows:
+1. set FLASK_APP=main
+2. flask run
+For Mac:
+1. export FLASK_APP=main.py
+2. flask run
---
+++ env/app/models/employee.py
@@ -0,0 +1,5 @@
+class Employee:
+    def __init__(self, Name):
+        self.ID = 0
+        self.Name = Name
+        self.Status = 0
