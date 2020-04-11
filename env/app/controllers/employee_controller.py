4b6c4dc11122a0705d363ea021a46bdd706491a4
thien-pham <thien.pham@be.xyz>
2020-04-12 02:50:04 +0700

    fix bug add new

--- env/app/controllers/employee_controller.py
+++ env/app/controllers/employee_controller.py
@@ -7,13 +7,14 @@ def getEmployee(form):
     return Employee(form.Name.data, form.CMND.data, form.Phone.data, form.Company.data, form.Reason.data, form.DueDate.data, form.Image.data, 0, datetime.now(), 0, datetime.now())
 
 def index():
-    form = EmployeeForm(None)
+    form = EmployeeForm()
     if form.validate_on_submit():
         if Employee.insertEmployee(getEmployee(form)):
             flash('Nhân viên {}, số CMND: {} khởi tạo thành công.'.format(form.Name.data, form.CMND.data))
         else:
             flash('Có gì đó sai sai.')
         return redirect('/')
+    print(form.DueDate.data)
     return render_template('employee/index.html', form=form)
 
 def list():
