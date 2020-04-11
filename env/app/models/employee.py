47b86e3efea8ae61233732a4f4181909ee209e44
MaiMai <anh.vu2@tiki.vn>
2020-04-11 23:20:07 +0700

    add get one

--- env/app/models/employee.py
+++ env/app/models/employee.py
@@ -25,40 +25,40 @@ class Employee:
 
     @staticmethod
     def createNewID():
-        # import pdb
-        # pdb.set_trace()
+        import pdb
+        pdb.set_trace()
         with open(CURRENT_WD + '/datas/index.txt', 'r' ) as f:
-            current_index = f.read()
-            if current_index =='':
+            __current_index = f.read()
+            
+            if __current_index =='':
                 id = 0
             else:
-                id = int(current_index) + 1
-
-        with open(CURRENT_WD + '/datas/index.txt', 'w+' ) as f:
-            f.write(str(id))
+                __current_index_json = json.loads(__current_index)
+                id = int(__current_index_json['Employee']) + 1
         return id 
 
     @staticmethod
     def insertEmployee(employee):
-        # import pdb
-        # pdb.set_trace()
         if employee.ID != 0:
             return False
         employee.ID = Employee.createNewID()
         employee.Status = Status.INIT.value
-        employee.DueDate = employee.DueDate.strftime("%Y-%m-%d %H:%M:%S")
+        employee.DueDate = employee.DueDate.c("%Y-%m-%d %H:%M:%S")
         employee.CreatedAt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         employee.CreatedAt = employee.CreatedAt
         data = json.dumps(employee.__dict__)
         with open(LOCAL_FILE, 'a+') as f:
             f.write(data) 
             f.write('\n')
+        with open(CURRENT_WD + '/datas/index.txt', 'w+' ) as f:
+            f.write({'Employee': str(employee.ID)})
         return True
 
     # thêm CreatedAt 
     @staticmethod
-    def getEmployeesByStatus(status):
-        data = []
+    def getEmployeesByStatus(statuses):
+        # for status in statuses:
+        
         with open(LOCAL_FILE, 'r') as f:
             data = f.read()
             result  = [] 
@@ -69,41 +69,55 @@ class Employee:
 
 # thêm updated at 
     @staticmethod
-    def getEmployeeByID(id):
-        # import pdb
-        # pdb.set_trace()
+    def getEmployeeByID(id): # get last row 
+        
         with open(LOCAL_FILE, 'r') as f:
             for line in f:
                 row = json.loads(line)
                 if row.get('ID') == id:
-                    result = Employee(**row)
+                    result = row
         return result
 
-    @staticmethod
-    def updateEmployeeByID(employee): # check employee status  # convert to another type # update # save again
+    # @staticmethod
+    # def updateEmployeeByID(employee): # check employee status  # convert to another type # update # save again
 
-        existing_employee = Employee.getEmployeeByID(employee.ID)
-        updated_info = employee.__dict__
-        try: 
-            if updated_info.Status in (1,2) and existing_employee.ID in (0):
-                return -1 
-        except:
-            pass
-        for k, v in updated_info.items():
-            if hasattr(existing_employee, k) and v is not None:
-                    setattr(existing_employee, k, v)
-        import pdb
-        pdb.set_trace()
-        existing_employee.UpdatedAt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        row_to_edit = json.dumps(existing_employee.__dict__) 
-        # cách 1: 
-        with open (LOCAL_FILE, 'r') as read_file, open(LOCAL_FILE.replace('.json', '_new.json'), 'w') as write_file:
-            for row in read_file:
-                if row['ID'] == employee.ID:
-                    write_file.write(row_to_edit)
-                else:
-                    write_file.row()
-        return 0 
+    #     existing_employee = Employee.getEmployeeByID(employee.ID)
+    #     updated_info = employee.__dict__
+    #     try: 
+    #         if updated_info.get('Status') in (1,2) and existing_employee.ID in (0):
+    #             return -1 
+    #     except:
+    #         pass
+    #     for k, v in updated_info.items():
+    #         if hasattr(existing_employee, k) and v is not None:
+    #                 setattr(existing_employee, k, v)
+    #     # import pdb
+    #     # pdb.set_trace()
+
+    #     existing_employee.UpdatedAt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #     row_to_edit = json.dumps(existing_employee.__dict__) 
+    #     temp = [] 
+    #     with open (LOCAL_FILE, 'r') as read_file: 
+    #         for row in read_file:
+    #             __dict_row = json.loads(row)
+    #             if __dict_row['ID'] == employee.ID:
+    #                 temp.append(row_to_edit)
+    #             else:
+    #                 temp.append(__dict_row)
+    #     print(temp)
+    #     with open (LOCAL_FILE, 'w+') as f:  
+    #         for line in  temp:
+    #             f.write(line)
+    #             f.write('\n')
+    #     return 0 
+
+    @staticmethod
+    def updateStatusEmployee(id, new_status):
+        if new_status not in (1,2):
+            return False
+        else:
+            with open(LOCAL_FILE, 'r') as f:
+
 
 
     
@@ -123,9 +137,10 @@ if __name__ == "__main__":
                     'Reason': 'new', 
                     'DueDate': '2020-03-04', 
                     'Image': None }
-    # print(type(__test_dict))
+    print(type(__test_dict))
     Employee.insertEmployee(Employee(**_new_emp))
-    maianh_07 = Employee(**__test_dict)
+    # maianh_07 = Employee(**__test_dict)
+    # print(maianh_07)
     # print(maianh_07)
     # print(maianh_07.ID)
     # print(maianh)
